d4917bdea54e7146bae4bb3129c862f18ec7689b
GarbageCollecterH4x <ealmaguer@eagles.ewu.edu>
2019-11-06 11:14:05 -0800

    Fresh start.

---
+++ src/Player.java
@@ -0,0 +1,14 @@
+//team: garbage collector
+
+public class Player{
+
+    private String      name;
+
+
+    public Player(String p_name){
+
+        this.name =     p_name;
+
+    }
+
+}
---
+++ src/TriviaQuestion.java
@@ -0,0 +1,21 @@
+//team: garbage collector
+
+public class TriviaQuestion{
+
+    private String      question;
+    private String      answer;
+    private String      hint;
+
+
+
+    public TriviaQuestion(String q, String a){
+
+        this.question = q;
+
+        this.answer =   a;
+
+    }
+
+
+
+}//end class TriviaQuestion
